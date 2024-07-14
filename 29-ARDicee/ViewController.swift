970ea1ba4ecdff9bd4f5ba1656c72fdde9a0294a
Nour <mohamednour25@gmail.com>
2024-07-14 13:09:18 +0300

    03

--- 29-ARDicee/ViewController.swift
+++ 29-ARDicee/ViewController.swift
@@ -18,27 +18,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
-        //crete sphere
-        let sphere = SCNSphere(radius: 0.2)
-        // make color
-        let material = SCNMaterial()
-        material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
-        // assing the color to the sphere
-        sphere.materials = [material]
-        // make position
-        let node = SCNNode()
-        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
-        // assing the sphere too that postion created earlier
-        node.geometry = sphere
-        // assing the node to the scene
-        sceneView.scene.rootNode.addChildNode(node)
+        
         sceneView.autoenablesDefaultLighting = true
         
-//        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//        
-//        // Set the scene to the view
-//        sceneView.scene = scene
+        // Create a new scene
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada copy.scn")!
+        if let dicNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            dicNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+            sceneView.scene.rootNode.addChildNode(dicNode)
+        }
+
+
+                                                      
     }
     
     override func viewWillAppear(_ animated: Bool) {
